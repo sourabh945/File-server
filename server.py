c3bdc4e5d6923186bd45c9cf7cbb518e33664e2f
Sourabh <sourabhsheokand945@gmail.com>
2024-06-16 19:39:16 +0530

    modified:   Template/share page/index.html 	deleted:    __pycache__/server.cpython-310.pyc 	modified:   logs/user_logs.csv 	new file:   modules/__pycache__/random_secret.cpython-310.pyc 	new file:   modules/random_secret.py 	modified:   server.py 	modified:   static/css for sharepage/main.css

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask , request , redirect , render_template , send_file , send_from_directory
+from flask import Flask , request , redirect , render_template , send_file , url_for
+
 
 ################ modules imports ##############
 
@@ -7,16 +8,23 @@ from modules.user import users_module
 from modules.errors import error_logs
 from modules.folder_operator import ls
 from modules.folder_selector import get_folder
+from modules.random_secret import secret_generator , secret_set
 
 ############ usr defined parameters ############
 
 re_login_time = 1 ### time after re-login is required ( in hours )
-folder_path = get_folder()
+initial_folder_path = get_folder()
+
+##############################################
+
+parser = dict()
 
 ##############################################
 
 app = Flask(__name__,template_folder="Template")
 app.config["SESSION_PERMANENT"] = False
+
+##############################################
 
 ##############################################
 
@@ -34,7 +42,7 @@ def authenticate_user(username,password,ipaddress) -> any:
 
 def validate_folder(folder:str) -> bool:
     daughter_folder = folder.split("/")
-    parent_folder = folder_path.split("/")
+    parent_folder = initial_folder_path.split("/")
     for i in range(len(parent_folder)):
         if parent_folder[i] != daughter_folder[i]:
             return False
@@ -65,15 +73,24 @@ def login():
         ip_address = request.remote_addr
         status, user = authenticate_user(username,password,ip_address)
         if status is True:
-            return file_explorer(user,folder_path)
+            secret = secret_generator(16)
+            parser[secret] = [user,initial_folder_path]
+            return redirect(url_for('.file_explorer',parser_key=secret))
         else:
-            return render_template("./login form/index.html", login="False")
+            return render_template("login form/index.html", login="False")
     return render_template("login form/index.html",login="True")
 
 ################################################
 
 @app.route("/file",methods=['GET','POST'])
-def file_explorer(user,folder_path):
+def file_explorer():
+    try:
+        secret = request.args['parser_key']
+        user , folder_path = parser[secret]
+        del parser[secret]
+        secret_set.remove(secret)
+    except:
+        return redirect('/login')
     if user.validate_user() is True:
         if request.method == "POST":
             item_type = request.form.get("item_type")
@@ -81,30 +98,47 @@ def file_explorer(user,folder_path):
             parent_folder = request.form.get("parent_folder")
             if item_type == "dir":
                 if validate_folder(parent_folder+item_name) is True:
-                    return file_explorer(user,folder_path=parent_folder+item_name)
+                    secret = secret_generator(16)
+                    parser[secret] = [user,parent_folder+item_name]
+                    return redirect(url_for('.file_explorer',parser_key=secret))
             elif item_type == "file":
                 if validate_folder(parent_folder) is True:
-                    return send_file_to_client(user,folder_path+item_name)
+                    secret = secret_generator(16)
+                    parser[secret] = [user,parent_folder+item_name]
+                    return redirect(url_for('.send_file_to_client',parser_key=secret))
             elif item_type == "up_dir":
                 if validate_folder(up_folder_path(parent_folder)) is True:
-                    return file_explorer(user,folder_path=up_folder_path(parent_folder))
+                    secret = secret_generator(16)
+                    parser[secret] = [user,up_folder_path(parent_folder)]
+                    return redirect(url_for('.file_explorer',parser_key=secret))
             else:
                 return render_template("share page/index.html",content=ls(folder_path),username=user.username,session_id=user.session_id,parent_folder=folder_path)
-        else:
-            return render_template("share page/index.html",content=ls(folder_path),username=user.username,session_id=user.session_id,parent_folder=folder_path)
+        elif request.method == "GET":
+            secret = secret_generator(16)
+            parser[secret] = [user,folder_path]
+            return render_template("share page/index.html",content=ls(folder_path),username=user.username,session_id=user.session_id,parent_folder=folder_path,secret=secret)
         
 ###################################################
 
-@app.route('/download_file/{file_name}')
-def send_file_to_client(user,file_path):
+@app.route('/download_file')
+def send_file_to_client():
+    try:
+        secret = request.args['parser_key']
+        user , file_path = parser[secret]
+        del parser[secret]
+        secret_set.remove(secret)
+    except:
+        return redirect("/login")
     try:
         if user.validate_user() is True:
             return send_file(file_path)
     except Exception as error:
         error_logs(error,send_file_to_client)
-        return file_explorer(user,folder_path)
+        secret = secret_generator(16)
+        parser[secret] = [user,initial_folder_path]
+        return redirect(url_for('.file_explorer',parser_key=secret))
     
 #################################################
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
